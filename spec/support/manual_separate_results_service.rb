a314069d62492c543fea0891b55a53c5092ea544
Jakub Żuchowski <ellmunzai@gmail.com>
2020-02-25 20:27:36 +0100

    fixing spec template without arguments
    bumping to 0.3.3

--- spec/support/manual_separate_results_service.rb
+++ spec/support/manual_separate_results_service.rb
@@ -4,7 +4,7 @@ class ManualSeparateResultsService < PiperService
   pipe :manual_spearate_results do
     capital_letters = input.scan(/[A-Z]/)
 
-    mssg     { "input has capital letters" }
+    mssg        { "input has capital letters" }
     cond        { capital_letters.empty? }
     fail_object { capital_letters.join(", ") }
     object      { input }
